77dac3256c9a342ab6faab0c390343ed94155483
bosungpark <qkrqhtjd0806@naver.com>
2022-01-29 16:29:23 +0900

    study about component scan

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,56 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("di container without spring")
+    void pureContainer(){
+        AppConfig appConfig=new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+
+    }
+
+    @Test
+    @DisplayName("use object that apply singlethon pattern")
+    void singletonServiceTest(){
+        SingletonService singletonService1= SingletonService.getInstance();
+        SingletonService singletonService2= SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("Spring container and Singleton")
+    void springContainer(){
+
+        ApplicationContext ac= new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService",MemberService.class);
+
+        MemberService memberService2 = ac.getBean("memberService",MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+    }
+}
